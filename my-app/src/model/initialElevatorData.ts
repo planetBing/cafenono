2dca3b3e795f832d210a87b37461c2a886a1b0d9
정채영 <sarah100217@gmail.com>
2024-06-24 23:37:16 +0900

    refactor: initialElevatorData 분리

---
+++ my-app/src/model/initialElevatorData.ts
@@ -0,0 +1,19 @@
+import { ElevatorType } from "./types";
+
+export const initialElevatorData: ElevatorType[] = [
+  {
+    id: 1,
+    destinationFloor: 1,
+    moving: false,
+  },
+  {
+    id: 2,
+    destinationFloor: 1,
+    moving: false,
+  },
+  {
+    id: 3,
+    destinationFloor: 1,
+    moving: false,
+  },
+];
